b89e7143e30866332b0d551b04a7180c48c6dfb7
Khubaibkhan1 <141897448+Khubaibkhan1@users.noreply.github.com>
2023-09-25 16:57:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+                                                  chapter 1\\
+                                                    alert\\
+ Q1
+ alert('hello from script tag'); 
+ Q2
+ alert('Error: Please enter a valid password'); 
+ Q3
+ alert('Welcome to js land... \n Happy coding'); 
+ Q4a
+ alert('Welcome to js land...');
+ Q4b
+ alert('Happy coding');
+ Q5
+ alert('Hello... I can run JS through my web browsers s console');
+Q6
+   document.getElementById('showAlert').addEventListener('click', function() {
+    alert('Hello! This is an alert from My Project.');
+});
