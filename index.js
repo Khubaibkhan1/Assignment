33053d0f0f6d4171a0e66a9520d122e85f350abd
Khubaibkhan1 <141897448+Khubaibkhan1@users.noreply.github.com>
2023-09-25 17:13:53 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-                                                  chapter 1\\
-                                                    alert\\
- Q1
- alert('hello from script tag'); 
- Q2
- alert('Error: Please enter a valid password'); 
- Q3
- alert('Welcome to js land... \n Happy coding'); 
- Q4a
- alert('Welcome to js land...');
- Q4b
- alert('Happy coding');
- Q5
- alert('Hello... I can run JS through my web browsers s console');
-Q6
-   document.getElementById('showAlert').addEventListener('click', function() {
-    alert('Hello! This is an alert from My Project.');
-});
+                                                   // chapter 2 //
+                                              // variables for string //
+// 1. declare var
+var ans; 
+
+// 2. assign var
+ans = "taha";
+
+// 3.
+var message;
+message = "Hello World";
+alert("Welcome to " + message);
+
+// 4.
+var student = "John";
+var age = "20 year old";
+var passion = "Cerfitifed Moblie Appliction";
+alert(student);
+alert(age);
+alert(passion);
+
+// 5.
+var taha = "pizza";
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// 6.
+var email;
+email = "example@example.com";
+alert("My email address is " + email);
+
+// concatenation alert("my abc xyz " + anymessage);
+
+// 7.
+var book = "A smarter way to learn javascript";
+alert("The book is " + book);
+
+// 8. 
+document.getElementById("anas").innerHTML = "Yah! I can write HTML content through JavaScript";
+
+// 9.
